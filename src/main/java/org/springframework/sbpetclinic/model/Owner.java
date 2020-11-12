0d886b2783abdad2afcd810ea7502be08c93e8d5
Rakesh Nair <Rakesh.Nair@accelya.local>
2020-11-12 18:16:03 +0530

    Adding POJOs. Closes #2

---
+++ src/main/java/org/springframework/sbpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package org.springframework.sbpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/org/springframework/sbpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package org.springframework.sbpetclinic.model;
+
+public class Vet extends Person {
+
+}
