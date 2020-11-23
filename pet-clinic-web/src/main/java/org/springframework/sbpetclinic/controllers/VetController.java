2d5cf4c011342933245b1e7dd21a9edf675be9f7
Rakesh Nair <Rakesh.Nair@accelya.local>
2020-11-23 16:07:44 +0530

    Added Vets List. Closes #15

--- pet-clinic-web/src/main/java/org/springframework/sbpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/springframework/sbpetclinic/controllers/VetController.java
@@ -1,12 +1,24 @@
 package org.springframework.sbpetclinic.controllers;
 
+import org.springframework.sbpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+	public VetService vetService;
+	
+	
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+
 	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", this.vetService.findAll());
 		return "vets/index";
 	}
 }
