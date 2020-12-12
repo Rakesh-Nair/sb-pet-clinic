b44cb3b0ff0df6999a6d89cc9de78c2ad298845d
Rakesh Nair <Rakesh.Nair@accelya.local>
2020-12-12 19:49:38 +0530

    Added visit entity. Closes #26

--- pet-clinic-data/src/main/java/org/springframework/sbpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/springframework/sbpetclinic/model/Owner.java
@@ -1,5 +1,22 @@
 package org.springframework.sbpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+	private Set<Pet> pets;
+
+	/**
+	 * @return the pets
+	 */
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	/**
+	 * @param pets the pets to set
+	 */
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 
 }
