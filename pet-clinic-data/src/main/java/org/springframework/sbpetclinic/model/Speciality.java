82f6d30e24f96b0a8c53d80052df48db1db4285f
Rakesh Nair <Rakesh.Nair@accelya.local>
2020-12-12 19:54:52 +0530

    Added speciality. Closes #27

---
+++ pet-clinic-data/src/main/java/org/springframework/sbpetclinic/model/Speciality.java
@@ -0,0 +1,20 @@
+package org.springframework.sbpetclinic.model;
+
+public class Speciality extends BaseEntity {
+	private String description;
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
--- pet-clinic-data/src/main/java/org/springframework/sbpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/springframework/sbpetclinic/model/Vet.java
@@ -1,5 +1,23 @@
 package org.springframework.sbpetclinic.model;
 
-public class Vet extends Person {
+import java.util.Set;
 
+public class Vet extends Person {
+	private Set<Speciality> specialities;
+
+	/**
+	 * @return the specialities
+	 */
+	public Set<Speciality> getSpecialities() {
+		return specialities;
+	}
+
+	/**
+	 * @param specialities the specialities to set
+	 */
+	public void setSpecialities(Set<Speciality> specialities) {
+		this.specialities = specialities;
+	}
+	
+	
 }
