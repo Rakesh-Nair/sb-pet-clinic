1bf0ada6048f7918d0cb7ad14bdc6ae2ea0ec6ba
Rakesh Nair <Rakesh.Nair@accelya.local>
2020-12-12 19:59:49 +0530

    Added contact properties. Closes #28

--- pet-clinic-data/src/main/java/org/springframework/sbpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/springframework/sbpetclinic/model/Owner.java
@@ -3,7 +3,11 @@ package org.springframework.sbpetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+	
 	private Set<Pet> pets;
+	private String telephone;
+	private String address;
+	private String city;
 
 	/**
 	 * @return the pets
@@ -17,6 +21,48 @@ public class Owner extends Person {
 	 */
 	public void setPets(Set<Pet> pets) {
 		this.pets = pets;
+	}
+
+	/**
+	 * @return the telephone
+	 */
+	public String getTelephone() {
+		return telephone;
+	}
+
+	/**
+	 * @param telephone the telephone to set
+	 */
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
 	}
 
 }
