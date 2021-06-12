ec97a7f769b7dec6f5d0e94b618cff0caa77b918
Rakesh Nair <crucifyrakesh@gmail.com>
2021-06-12 19:54:36 +0530

    Update PetType. #56

--- pet-clinic-data/src/main/java/org/springframework/sbpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/org/springframework/sbpetclinic/model/PetType.java
@@ -4,7 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,14 +14,19 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class PetType extends BaseEntity {
+
 	@Column(name = "name")
 	private String name;
 
 	@Override
 	public String toString() {
 		return name;
+	}
+
+	@Builder
+	public PetType(Long id, String name) {
+		super(id);
+		this.name = name;
 	}
 }
